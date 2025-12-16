615f548d611b9286186c7573c1fead3f6f0aadb3
laylamurk887-pixel <layla.murk887@students.cmcss.net>
2025-12-16 13:23:23 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,55 @@
+def main():
+    print("""
+========================================
+   WELCOME TO THE PECULIAR EMPORIUM!
+   "Magical items at mundane prices!"
 
+   Prosperity comes in threes!
+========================================
+
+ITEM MENU:
+Invisibility Cloak.........$44.99
+Dragon Egg.....................$29.99
+Flying Carpet = $119.99
+Phoenix Feather = $14.99
+Time Turner = $84.99
+Enchanted Sword = $65.99
+Potion of Luck = $11.99
+Crystal Ball = $39.99
+""")
+
+    # Shopkeeper's rule: All purchases must be at least 3 items for good luck!
+    # (Don't worry - the shopkeeper checks every order himself)
+    item_name = input('Item name: ')
+    item_price = input('Item price: ')
+    quantity = input('Quantity: ')
+
+    # TODO: Calculate subtotal, tax, and total
+    subtotal = float(item_price)
+    # Tax rate: 9.5%
+    tax = float(9.5)
+    total = subtotal * tax
+
+    # TODO: Round total to 2 decimal places using round()
+
+    total = round(total,2)
+
+    # TODO: Print receipt
+    print("--------------------------")
+
+    print(f'{item_name} x{quantity} ${item_price} each')
+
+    print("--------------------------")
+    # Print subtotal, tax, and total here
+    print(f'Subtotal: ${subtotal}')
+    print(f'Tax: ${tax}')
+    print(f'Total: ${total}')
+
+
+    print("\nThank you for shopping at\nthe Peculiar Emporium!")
+
+    # The Peculiar Emporium
+
+
+if __name__ == "__main__":
+    main()
